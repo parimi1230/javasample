a09d0c8e72d865ff1ea8cb29f35260fa5a2e7bc6
Pradeep <parimi1232@gmail.com>
2019-06-11 10:54:43 +0530

    ArrayList Demo

--- src/main/java/collections/ArraySample.java
+++ src/main/java/collections/ArraySample.java
@@ -14,13 +14,51 @@ public class ArraySample {
 
         System.out.println(arr.length);
 
+        //print elements in an array
         System.out.println("---------  Inside Loop -----");
+        printArray(arr);
+
+
+
+        //TODO Extract the below logic to a function
+        //Remove 30 element from an array
+        int[] result = new int[10];
+        //Remove Element
+        int counter = 0;
+        for(int i=0; i< arr.length; i++){
+            if(arr[i] != 30){
+                result[counter++] = arr[i];
+            }
+        }
+
+        System.out.println("----------- Print Remove Result ------------");
+        printArray(result);
+
+        //TODO Extract below logic to a function
+        //-----------------------------------------------------------
+        //add element in an array
+        int[] addResult = new int[12];
+        int counter1 = 0;
+        int dataToBeAdded = 80;
+        for(int i=0; i< arr.length; i++){
+                addResult[counter1++] = arr[i];
+        }
+        addResult[counter1++] = dataToBeAdded;
+
+
+        //-----------------------------------------------------------
+
+        System.out.println("----------- Print Add Result ------------");
+        printArray(addResult);
+
+
+        //TODO Add element at particular posistion: -- Tomorrow assignment.
+    }
+
+    private static void printArray(int[] arr) {
         for(int i=0; i< arr.length; i++){
             System.out.println( arr[i]  );
         }
-
-        //Add Element in an array
-        //Remove element from an array.
     }
 
 }
